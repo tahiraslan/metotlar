ec5a02047c8f26f38927cc175e0ba7cbec76a2cd
Tahir Aslan <tahiraslan@ymail.com>
2022-07-26 14:36:25 +0300

    master

--- src/main.java
+++ src/main.java
@@ -1,13 +1,19 @@
 import java.awt.*;
+import java.security.PublicKey;
 import java.util.Scanner;
 import java.util.Random;
 public class Main {
     public static void main(String[] args){
 
-
+    Main metotlar=new Main();
+    metotlar.deneme();
+    deneme2();
 
     }
     public void deneme(){
         System.out.println("bu bir deneme metotudur.");
+    }
+    public static void deneme2(){
+        System.out.println("bu bir static metotdur");
     }
 }
