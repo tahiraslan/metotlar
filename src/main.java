ade197f55df3cce6f822485a397dafb5c053a74f
Tahir Aslan <tahiraslan@ymail.com>
2022-07-26 14:32:03 +0300

    tahir aslan

--- src/main.java
+++ src/main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class main {
+import java.awt.*;
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args){
+
+
+
+    }
+    public void deneme(){
+        System.out.println("bu bir deneme metotudur.");
+    }
 }
